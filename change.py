ab8853f20ab4f1c89d1b5c0b9fac5938594cea4d
long <long@example.com>
2021-06-03 11:22:45 +0800

    ok

--- change.py
+++ change.py
@@ -2,6 +2,51 @@
 import sys 
 from PIL import Image
 
+from Crypto.Cipher import AES  
+from base64 import b64decode, b64encode
+
+
+import argparse
+
+
+
+
+BLOCK_SIZE = AES.block_size
+# 不足BLOCK_SIZE的补位(s可能是含中文，而中文字符utf-8编码占3个位置,gbk是2，所以需要以len(s.encode())，而不是len(s)计算补码)
+pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * (BLOCK_SIZE - len(s) % BLOCK_SIZE).to_bytes(1,'little')
+# 去除补位
+unpad = lambda s: s[:-ord(s[len(s) - 1:])]
+
+
+class AESCipher:
+    def __init__(self, secretkey):
+        self.key = secretkey  # 密钥
+        self.iv = secretkey[0:16]  # 偏移量
+
+    def encrypt(self, text):
+        """
+        加密 ：先补位，再AES加密，后base64编码
+        :param text: 需加密的明文
+        :return:
+        """
+        # text = pad(text) 包pycrypto的写法，加密函数可以接受str也可以接受bytess
+        text = pad(text) # 包pycryptodome 的加密函数不接受str
+        cipher = AES.new(key=self.key, mode=AES.MODE_CBC, IV=self.iv)
+        encrypted_text = cipher.encrypt(text)
+        # 进行64位的编码,返回得到加密后的bytes，decode成字符串
+        return encrypted_text#b64encode(encrypted_text).decode('utf-8')
+
+    def decrypt(self, encrypted_text):
+        """
+        解密 ：偏移量为key[0:16]；先base64解，再AES解密，后取消补位
+        :param encrypted_text : 已经加密的密文
+        :return:
+        """
+        #encrypted_text = b64decode(encrypted_text)
+        cipher = AES.new(key=self.key, mode=AES.MODE_CBC, IV=self.iv)
+        decrypted_text = cipher.decrypt(encrypted_text)
+        return unpad(decrypted_text)
+
 
 def make_even_image(image):
     """取得一个 PIL 图像并且更改所有值为偶数，使最低有效位为 0
@@ -34,7 +79,7 @@ def encode_data_in_image(image, data):
     # 将需要隐藏的字符串转换成二进制字符串
     # 每个字符转换成二进制之后，对应一个或多个字节码
     # 每个字节码为一个十进制数值，将其转换为 8 为二进制字符串后相加
-    binary = ''.join(map(int_to_binary_str, bytearray(data, 'utf-8'))) 
+    binary = ''.join(map(int_to_binary_str, data))
     # 每个像素点的 RGBA 数据的最低位都已经空出来，分别可以存储一个二进制数据
     # 所以图片可以存储的最大二进制数据的位数是像素点数量的 4 倍
     # 如果需要隐藏的字符串转换成二进制字符串之后的长度超过这个数，抛出异常
@@ -67,41 +112,14 @@ def binary_to_string(binary):
     # index 是 binary 的索引，初始值为 0
     index = 0
     # 创建空列表，以用于添加解析得到的人类能够看懂的 UTF-8 字符
-    strings = []
+    hide_bytes = b''
+    while index < len(binary):
+        bits8 = binary[index:index+8]
+        int8 = int(bits8,2)
+        hide_bytes+=int8.to_bytes(1,'little')
+        index += 8
 
-    # 创建嵌套函数，参数为二进制字符串片段和字节数，返回值为有效二进制字符串
-    def effective_binary(binary_part, zero_index):
-        if not zero_index:
-            return binary_part[1:]
-        binary_list = []
-        for i in range(zero_index):
-            small_part = binary_part[8 * i: 8 * i + 8]
-            binary_list.append(small_part[small_part.find('0') + 1:])
-        return ''.join(binary_list)
-
-    while index + 1 < len(binary):
-        # 字符所占字节数举例：
-        # 字符 'a' 转换为字节码后，占 1 个字节，且字节的最高位是 0
-        # 字符 '哈' 转换为字节码后，占 3 个字节，且第一个字节的最高位是 1110
-        # 占 1 个字节，开头是 0xxxxxxx
-        # 占 2 个字节，开头是 110xxxxx
-        # 占 3 个字节，开头是 1110xxxx
-        # 占 4 个字节，开头是 11110xxx
-        # zero_index 为第一个 ‘0’ 的索引，根据它我们可以知道字节数
-        zero_index = binary[index:].index('0') 
-        # 单个字符转换为二进制字节码后的长度：字节数乘以 8
-        length = zero_index * 8 if zero_index else 8
-        # 调用 effective_binary 函数，参数有两个：binary 片段，字节数
-        # 返回的是有效二进制字符串，该字符串通过 int 方法转换成十进制数值
-        # 然后使用 chr 方法即可获得对应的字符啦
-        string = chr(int(effective_binary(
-                binary[index: index+length], zero_index), 2))
-        # 将字符添加到字符列表 strings 中
-        strings.append(string)
-        # 索引变量增加已经处理的二进制字符串长度
-        index += length
-    # 最后返回解析得到的字符串
-    return ''.join(strings)
+    return hide_bytes
 
 
 def decode_data_from_image(image):
@@ -109,9 +127,9 @@ def decode_data_from_image(image):
     '''
     # 从图片的像素点数据中获得存储数据的二进制字符串
     binary = ''.join([bin(r)[-1] + bin(g)[-1] + bin(b)[-1] + bin(a)[-1]
-            for r, g, b, a in image.getdata()])
-    # 出现连续 16 个 0 的字符串片段的索引判定为有效数据截止处
-    many_zero_index = binary.find('0' * 16)
+            for r, g, b, a in image.getdata()]).encode()
+    # 出现连续 32 个 0 的字符串片段的索引判定为有效数据截止处
+    many_zero_index = binary.find(b'0' * 32)
     # 有效数据字符串的长度一定是 8 的倍数
     # 以此判定准确的断点索引，获得有效数据的二进制字符串
     end_index = (many_zero_index + 8 - many_zero_index % 8 
@@ -119,22 +137,48 @@ def decode_data_from_image(image):
     data = binary_to_string(binary[:end_index])
     return data
 
+def encrpt_zip(zip_file,mypass):
+    myzip_fd = open(zip_file,'rb')
+    contents = myzip_fd.read() #bytes 160
+    #print(repr(contents))
+    myenc = AESCipher(mypass)
+    myzip_fd.close()
+    return myenc.encrypt(contents)
+
+def decrpt_zip(zip_file,mypass,contents):
+    myzip_fd = open(zip_file,'wb')
+    myenc = AESCipher(mypass)
+    decrpt_data=myenc.decrypt(contents)
+    myzip_fd.write(decrpt_data)
+    myzip_fd.close()
+    return
 
 def main():
-    '''主函数
-    '''
-    # 获取原图片文件和新图片文件的名字
-    image_file, new_image_file = sys.argv[1:]
-    # 调用 Image 的 open 方法获取原图片对象
-    image = Image.open(image_file)
-    # 需要隐藏到图片中的字符串
-    str_to_hide = 'Hello World!'
-    # 调用此函数生成包含字符串数据的新图片对象
-    new_image = encode_data_in_image(image, str_to_hide)
-    # 将新图片对象保存到新文件里
-    new_image.save(new_image_file)
-    # 调用此函数获取隐藏在新图片对象中的字符串并打印
-    print(decode_data_from_image(new_image))
+    parser = argparse.ArgumentParser(description="change")
+    parser.add_argument('-z','--zip',help='zip')
+    parser.add_argument('-p','--png',help='png')
+    parser.add_argument('-m', '--mode', default='encode', help='encode or decode.')
+    parser.add_argument('-o','--output',default='encodeImage.png',help='name of output picture.')
+    args = parser.parse_args()
+    myzip = args.zip
+    image_file = args.png
+    mode = args.mode
+    new_image_file=args.output
+    if mode == 'encode':
+        contents_to_hide=encrpt_zip(myzip,b'WfgOMWDZeqB7vhxU')
+        # 调用 Image 的 open 方法获取原图片对象
+        image = Image.open(image_file)
+        # 调用此函数生成包含字符串数据的新图片对象
+        new_image = encode_data_in_image(image, contents_to_hide)
+        # 将新图片对象保存到新文件里
+        new_image.save(new_image_file)
+    if mode == 'decode':
+        image = Image.open(image_file)
+        # 调用此函数获取隐藏在新图片对象中的字符串并打印
+        decode_data = decode_data_from_image(image)
+        decrpt_zip(myzip,b'WfgOMWDZeqB7vhxU',decode_data)
+
+
 
 
 if __name__ == '__main__':
